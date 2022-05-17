a81f57aad9b3b4547812027f2d40f295776494f7
Elias Hernandez <me@eli.tf>
2022-05-17 15:02:16 -0400

    Added beforehand preview checking

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 	fmt.Println("NOAA NHC Atlantic Basin Tracker")
-	
+
 	if !checkEnv() {
 		fmt.Fprintln(os.Stderr, "Missing environment variables.")
 		return
@@ -25,14 +26,14 @@ func main() {
 	}
 
 	resp, err := http.Get("https://www.nhc.noaa.gov/gtwo.xml")
-	
+
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		return
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
-	
+
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		return
@@ -41,7 +42,7 @@ func main() {
 	var data NHCData
 
 	xml.Unmarshal(body, &data)
-	
+
 	items := data.Channel.Item
 
 	var basin NHCBasinItem
@@ -68,28 +69,43 @@ func main() {
 
 	fmt.Println("Atlantic Basin Tropical Outlook was last published on", datetime.Local().Format("Jan 02 2006, 3:04pm MST"))
 
-	if lastPublish == 0 {
-		fmt.Println("\"noaa-nhc:last-updated\" key not found in redis, setting to current publish date and sending webhook.")
-		err := setLastPublishDate(datetime.Unix()*1000)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			return
-		}
-	} else if lastPublish == datetime.Unix()*1000 {
-		fmt.Println("No new outlook information available.")
-		return
-	} else {
-		err := setLastPublishDate(datetime.Unix()*1000)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			return
-		}
-	}
+	previewURL := "https://www.nhc.noaa.gov/archive/xgtwo/atl/" + formatDate(datetime) + "/two_atl_5d0.png"
 
-	sendWebhook(os.Getenv("WEBHOOK_URL"), basin, datetime)
+	if lastPublish != datetime.Unix()*1000 {
+		if !verifyPreviewAvailable(previewURL) {
+			fmt.Println("New outlook information available but preview is not present.")
+			return
+		}
+
+		err := setLastPublishDate(datetime.Unix() * 1000)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			return
+		}
+
+		sendWebhook(os.Getenv("WEBHOOK_URL"), basin, datetime, previewURL)
+	} else {
+		fmt.Println("No new outlook information present, check again next time!")
+	}
 }
 
 func checkEnv() bool {
 	return os.Getenv("WEBHOOK_URL") != "" &&
-				 os.Getenv("REDIS_DSN") != ""
+		os.Getenv("REDIS_DSN") != ""
+}
+
+func formatDate(datetime time.Time) string {
+	return strings.Join(strings.Split(datetime.Format("2006 01 02 15 04"), " "), "")
+}
+
+func verifyPreviewAvailable(PreviewURL string) bool {
+	resp, err := http.Get(PreviewURL)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return false
+	}
+
+	return resp.StatusCode == 200
 }
--- notify.go
+++ notify.go
@@ -7,18 +7,17 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 )
-	
+
 type WebhookBody struct {
 	Embeds []EmbedBody `json:"embeds"`
 }
 
 type EmbedBody struct {
-	Title string `json:"title"`
-	URL   string `json:"url"`
-	Image Image `json:"image"`
+	Title  string `json:"title"`
+	URL    string `json:"url"`
+	Image  Image  `json:"image"`
 	Footer Footer `json:"footer"`
 }
 
@@ -30,16 +29,16 @@ type Footer struct {
 	Text string `json:"text"`
 }
 
-func sendWebhook(WebhookURL string, Basin NHCBasinItem, datetime time.Time) {
+func sendWebhook(WebhookURL string, Basin NHCBasinItem, datetime time.Time, PreviewURL string) {
 	data := &WebhookBody{
 		Embeds: []EmbedBody{
 			{
 				Title: "NHC Atlantic Outlook",
-				URL: Basin.Link,
+				URL:   Basin.Link,
 				Image: Image{
-					URL: "https://www.nhc.noaa.gov/archive/xgtwo/atl/" + formatDate(datetime) + "/two_atl_5d0.png",
+					URL: PreviewURL,
 				},
-				Footer: Footer{ 
+				Footer: Footer{
 					Text: "Last updated " + datetime.Local().Format("Jan 02 2006, 3:04pm MST"),
 				},
 			},
@@ -68,8 +67,4 @@ func sendWebhook(WebhookURL string, Basin NHCBasinItem, datetime time.Time) {
 	}
 
 	fmt.Println("Webhook message has been sent!")
-}
-
-func formatDate(datetime time.Time) string {
-	return strings.Join(strings.Split(datetime.Format("2006 01 02 15 04"), " "), "")
 }
